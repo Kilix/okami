2647076f107a459691797f501ccdc84fbd653fbe
wcastand <wcastand@gmail.com>
2017-08-24 11:09:56 +0200

    put component in dummy.js

--- stories/dummy.js
+++ stories/dummy.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import glamorous, {Div} from 'glamorous'
+import differenceInHours from 'date-fns/fp/differenceInHours'
 
 export const Container = glamorous.div({
   display: 'flex',
@@ -53,4 +54,41 @@ export const HourLabel = ({label, ...props}) =>
     padding={5}
     backgroundColor={props.idx % 2 ? '#FFF' : '#EAEAEA'}>
     {label}
+  </Div>
+
+const EventDiv = glamorous.div(
+  {
+    flex: 1,
+    zIndex: 99,
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    fontSize: 14,
+    padding: 5,
+    margin: '0 2px',
+    color: '#FFF',
+    overflow: 'hidden',
+    boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+    backgroundColor: '#F23543',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+  },
+  ({event}) => {
+    const delta = differenceInHours(event.start, event.end)
+    return {
+      visibility: event.title ? 'visible' : 'hidden',
+      height: delta > 1 ? 30 * delta - 10 : 20,
+    }
+  }
+)
+export const Event = ({event}) =>
+  event.render && event.end !== '*'
+    ? <EventDiv event={event} title={event.title}>
+        {event.title}
+      </EventDiv>
+    : <EventDiv event={{}} />
+
+export const NoEvent = props =>
+  <Div flex={1} alignSelf="stretch" textAlign="center" lineHeight="30px">
+    -
   </Div>
--- stories/sync.js
+++ stories/sync.js
@@ -2,49 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {storiesOf} from '@storybook/react'
 import glamorous, {Div} from 'glamorous'
-import differenceInHours from 'date-fns/fp/differenceInHours'
 
 import Calendar, {WeeklyCalendar} from '../src/'
 
-import {Container, HourLabel, CalendarContainer, Cell, DayLabel} from './dummy'
 import data from './data'
-
-const EventDiv = glamorous.div(
-  {
-    flex: 1,
-    zIndex: 99,
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    fontSize: 14,
-    padding: 5,
-    margin: '0 2px',
-    color: '#FFF',
-    overflow: 'hidden',
-    boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
-    backgroundColor: '#F23543',
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-  },
-  ({event}) => {
-    const delta = differenceInHours(event.start, event.end)
-    return {
-      visibility: event.title ? 'visible' : 'hidden',
-      height: delta > 1 ? 30 * delta - 10 : 20,
-    }
-  }
-)
-const Event = ({event}) =>
-  event.render && event.end !== '*'
-    ? <EventDiv event={event} title={event.title}>
-        {event.title}
-      </EventDiv>
-    : <EventDiv event={{}} />
-
-const NoEvent = props =>
-  <Div flex={1} alignSelf="stretch" textAlign="center" lineHeight="30px">
-    -
-  </Div>
+import {
+  Container,
+  HourLabel,
+  CalendarContainer,
+  Cell,
+  DayLabel,
+  Event,
+  NoEvent,
+} from './dummy'
 
 storiesOf('Sync', module).add('Basic', () =>
   <Calendar data={data} startingDay="monday" dateFormat="ddd DD/MM">

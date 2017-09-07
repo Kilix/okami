3ba778bfa272ac151aac9c9a50dc1e363b643014
wcastand <wcastand@gmail.com>
2017-09-07 12:12:38 +0200

    add Navigation component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Calendar from './calendar'
 import WeeklyCalendar from './components/weekly'
 import DailyCalendar from './components/daily'
 import MonthlyCalendar from './components/monthly'
+import Navigation from './components/navigation'
 
-export {Calendar, WeeklyCalendar, DailyCalendar, MonthlyCalendar}
+export {Calendar, WeeklyCalendar, DailyCalendar, MonthlyCalendar, Navigation}
 export default Calendar
